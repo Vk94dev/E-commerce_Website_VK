52e93f61837293d9bb1f893b093e1805571004d7
vivek <vivek94303@gmail.com>
2026-08-27 16:36:40 +0530

    correct code

--- server/config/firebaseAdmin.js
+++ server/config/firebaseAdmin.js
@@ -18,13 +18,13 @@
 
 
 import { initializeApp, cert } from "firebase-admin/app";
-import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
+// import serviceAccount from "./serviceAccountKey.json" with { type: "json" };
 
-// const serviceAccount = {
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-//   privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-// }
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+}
 
 initializeApp({
   credential: cert(serviceAccount),
